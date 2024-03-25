36c6c53a4f69631b2ed3a7acf0153ee47586879e
sayunlopchan <sayunlopchan01+github@gmail.com>
2024-03-26 00:56:26 +0545

    fixed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import RootLayout from './Ui/RootLayout'
+import Main from './Features/Home/Main'
+import About from './Features/Links/About'
+import Contact from './Features/Links/Contact'
+import Services from './Features/Links/Services'
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <RootLayout />,
+      children: [
+        { index: true, element: <Main /> },
+        { path: 'Main', element: <Main /> },
+        { path: 'About', element: <About /> },
+        { path: 'Services', element: <Services /> },
+        { path: 'Contact', element: <Contact /> },
+      ]
+    }
+  ])
+  return <RouterProvider router={router} />
 }
 
 export default App
