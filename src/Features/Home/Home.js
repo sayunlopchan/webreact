ee19aeaf28616e62c4336ef17f66a2078c1fcc2a
sayunlopchan <sayunlopchan01+github@gmail.com>
2024-03-27 03:03:04 +0545

    task 2 complete

--- src/Features/Home/Home.js
+++ src/Features/Home/Home.js
@@ -1,29 +1,13 @@
-import React, { useState } from 'react'
+import TaskA from './TaskA';
+import TaskB from './TaskB';
 
 const Home = () => {
-  const [count, countUpdate] = useState(0)
-
-  const handleAdd = () => {
-    return countUpdate(count + 1);
-  }
-
-  const handleMinus = () => {
-    return countUpdate(count - 1);
-  }
 
 
   return (
-    <div className='flex flex-col justify-center items-center h-[80vh] gap-3 font-mono'>
-      <div className='border-black border h-[200px] w-[200px] flex justify-center items-center'>
-        <span className='text-[40px] font-bold '>
-          {count}
-        </span>
-      </div>
-      <div className='space-x-4'>
-        <button onClick={handleAdd} className='bg-green-400 p-2 rounded-lg font-bold hover:bg-green-500'>Increment</button>
-
-        <button onClick={handleMinus} className='bg-red-400 p-2 rounded-lg font-bold hover:bg-red-500'>Decrement</button>
-      </div>
+    <div>
+      <TaskA />
+      <TaskB />
     </div>
   )
 }
---
+++ src/Features/Home/TaskA.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+
+const TaskA = () => {
+
+
+
+  const [count, countUpdate] = useState(0)
+
+  const handleAdd = () => {
+    return countUpdate(count + 1);
+  }
+
+  const handleMinus = () => {
+    return countUpdate(count - 1);
+  }
+  return (
+    <div className='flex flex-col justify-center items-center h-[50vh] gap-3 font-mono'>
+      <h1>Task 1</h1>
+      <div className='border-black border h-[200px] w-[200px] flex justify-center items-center'>
+        <span className='text-[40px] font-bold '>
+          {count}
+        </span>
+      </div>
+      <div className='space-x-4'>
+        <button onClick={handleAdd} className='bg-green-400 p-2 rounded-lg font-bold hover:bg-green-500'>Increment</button>
+
+        <button onClick={handleMinus} className='bg-red-400 p-2 rounded-lg font-bold hover:bg-red-500'>Decrement</button>
+      </div>
+    </div>
+  )
+}
+
+export default TaskA
