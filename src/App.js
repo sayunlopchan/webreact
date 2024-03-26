ca45e0969b9ff5d8ef8edbda277e1576ba4c2024
sayunlopchan <sayunlopchan01+github@gmail.com>
2024-03-27 00:49:44 +0545

    task , use useState and onClick to change update delete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import RootLayout from './Ui/RootLayout'
-import Main from './Features/Home/Main'
 import About from './Features/Links/About'
 import Contact from './Features/Links/Contact'
 import Services from './Features/Links/Services'
+import Home from './Features/Home/Home'
 
 const App = () => {
   const router = createBrowserRouter([
@@ -12,8 +12,7 @@ const App = () => {
       path: '/',
       element: <RootLayout />,
       children: [
-        { index: true, element: <Main /> },
-        { path: 'Main', element: <Main /> },
+        { index: true, element: <Home /> },
         { path: 'About', element: <About /> },
         { path: 'Services', element: <Services /> },
         { path: 'Contact', element: <Contact /> },
--- src/Ui/Header.js
+++ src/Ui/Header.js
@@ -1,17 +1,30 @@
 import { NavLink } from "react-router-dom"
+import { HiMiniBars4 } from "react-icons/hi2";
+import { ImCross } from "react-icons/im";
 
 const Header = () => {
   return (
-    <div className='flex justify-between bg-black py-5 px-3 text-white'>
-      <h1 className='font-mono font-extrabold'>
-        <NavLink to='Main'>Weblogo</NavLink>
-      </h1>
-      <nav>
-        <ul className='flex space-x-3 font-mono my-2'>
-          <NavLink to='About'>About</NavLink>
-          <NavLink to='Services'>Services</NavLink>
-          <NavLink to='Contact'>Contact</NavLink>
-        </ul>
+    <div className=' bg-black py-5 px-3 text-white'>
+      <div className="flex justify-between">
+        <h1 className='font-mono font-extrabold'>
+          <NavLink to='Main'>Weblogo</NavLink>
+        </h1>
+        <nav>
+          <ul className='flex space-x-3 font-mono my-2 max-sm:hidden'>
+            <NavLink to='About'>About</NavLink>
+            <NavLink to='Services'>Services</NavLink>
+            <NavLink to='Contact'>Contact</NavLink>
+          </ul>
+        </nav>
+        <div className="menubar flex sm:hidden items-center">
+          <HiMiniBars4 color="white" />
+        </div>
+      </div>
+
+      <nav className="flex flex-col font-mono sm:hidden">
+        <NavLink to='About'>About</NavLink>
+        <NavLink to='Services'>Services</NavLink>
+        <NavLink to='Contact'>Contact</NavLink>
       </nav>
     </div>
   )
