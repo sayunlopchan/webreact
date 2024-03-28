98418cda49364bb7b1d3359aecffedb006ad712d
sayunlopchan <sayunlopchan01+github@gmail.com>
2024-03-29 03:11:52 +0545

    showed data in Ui from Api using axios

--- src/Features/Home/Home.js
+++ src/Features/Home/Home.js
@@ -1,13 +1,45 @@
-import TaskA from "../../Task/TaskA"
-import TaskB from "../../Task/TaskB"
+import axios from "axios"
+import { useState } from "react";
+import TaskC from "../../Task/TaskC";
+
 
 const Home = () => {
 
 
+
+
+  // let [msg, setMsg] = useState(null);
+
+  // setTimeout(() => {
+  //   setMsg = ('hvdg')
+  //   console.log(msg + 'clg')
+  //   console.log(setMsg + 'clg')
+
+  // }, 1000)
+  // let [award, setAwards] = useState(null);
+
+  // const app = axios.get('http://www.omdbapi.com', {
+  //   params: {
+  //     title: 'Guardians of the Galaxy Vol. 2',
+  //     apikey: 'ca1bf188',
+  //     i: 'tt3896198',
+  //   }
+  // }).then((res) => {
+  //   setAwards(res.data.Awards);
+  //   console.log(res)
+  // }).catch((err) => {
+  //   console.log(err)
+  // })
+
+  // console.log(app)
+  // console.log('fvvvfvdvs')
+
+
+
+
   return (
     <div>
-      <TaskA />
-      <TaskB />
+      <TaskC />
     </div>
   )
 }
