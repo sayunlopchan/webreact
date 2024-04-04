bd6b958bb6e9ae6f28403eab37b2bbe80d45bfb7
sayunlopchan <sayunlopchan01+github@gmail.com>
2024-04-04 07:12:40 +0545

    changes

---
+++ src/Features/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import UseState from '../../Hooks/UseState'
+import UseEffect from '../../Hooks/UseEffect'
+
+const Home = () => {
+
+  return (
+    <div>
+      <UseState />
+      <UseEffect />
+    </div>
+  )
+}
+
+export default Home
---
+++ src/Hooks/UseEffect.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const UseEffect = () => {
+
+
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default UseEffect
