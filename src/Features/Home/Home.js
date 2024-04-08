06e7ab5482462e7d1dc3fff6c898a514c4ea606c
sayunlopchan <sayunlopchan01+github@gmail.com>
2024-04-08 16:09:24 +0545

    redux

--- src/Features/Home/Home.js
+++ src/Features/Home/Home.js
@@ -1,13 +1,11 @@
 import React from 'react'
-import UseStateHooks from '../../Hooks/UseStateHooks'
-import UseEffectHooks from '../../Hooks/UseEffectHooks'
 
 const Home = () => {
 
+
   return (
     <div>
-      <UseStateHooks />
-      <UseEffectHooks />
+
     </div>
   )
 }
--- src/Hooks/README.md
+++
@@ -1,7 +0,0 @@
-#Hooks
-
-1. what is hooks?
-
- Hooks are the functions to use some react features in the functional components.
-
-In other words, Hooks are the functions thet makes functional components works like Class components.  
